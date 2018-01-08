8588a210dc4e50865c90e0b3d17f2e86a11750f7
zhao <45147851@qq.com>
2018-01-08 16:07:24 +0800

    step 1 finished ... basics

---
+++ simulate_main_steps.py
@@ -0,0 +1,86 @@
+# Simulate (a Simon clone)
+# By Al Sweigart al@inventwithpython.com
+# http://inventwithpython.com/pygame
+# Released under a "Simplified BSD" license
+
+import random, sys, time, pygame
+from pygame.locals import *
+
+FPS = 30
+WINDOWWIDTH = 640
+WINDOWHEIGHT = 480
+
+BUTTONSIZE = 200
+BUTTONGAPSIZE = 20
+
+
+#                R    G    B
+WHITE        = (255, 255, 255)
+BLACK        = (  0,   0,   0)
+BRIGHTRED    = (255,   0,   0)
+RED          = (155,   0,   0)
+BRIGHTGREEN  = (  0, 255,   0)
+GREEN        = (  0, 155,   0)
+BRIGHTBLUE   = (  0,   0, 255)
+BLUE         = (  0,   0, 155)
+BRIGHTYELLOW = (255, 255,   0)
+YELLOW       = (155, 155,   0)
+DARKGRAY     = ( 40,  40,  40)
+
+bgColor = BLACK
+
+XMARGIN = int((WINDOWWIDTH - (2 * BUTTONSIZE) - BUTTONGAPSIZE) / 2)
+YMARGIN = int((WINDOWHEIGHT - (2 * BUTTONSIZE) - BUTTONGAPSIZE) / 2)
+
+# Rect objects for each of the four buttons
+YELLOWRECT = pygame.Rect(XMARGIN, YMARGIN, BUTTONSIZE, BUTTONSIZE)
+BLUERECT   = pygame.Rect(XMARGIN + BUTTONSIZE + BUTTONGAPSIZE, YMARGIN, BUTTONSIZE, BUTTONSIZE)
+REDRECT    = pygame.Rect(XMARGIN, YMARGIN + BUTTONSIZE + BUTTONGAPSIZE, BUTTONSIZE, BUTTONSIZE)
+GREENRECT  = pygame.Rect(XMARGIN + BUTTONSIZE + BUTTONGAPSIZE, YMARGIN + BUTTONSIZE + BUTTONGAPSIZE, BUTTONSIZE, BUTTONSIZE)
+
+def main():
+    global FPSCLOCK, DISPLAYSURF, BASICFONT
+
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    pygame.display.set_caption('Simulate')
+
+    BASICFONT = pygame.font.Font('freesansbold.ttf', 16)
+    infoSurf = BASICFONT.render('Match the pattern by clicking on the button or using the Q, W, A, S keys.', 1, YELLOW)
+    infoRect = infoSurf.get_rect()
+    infoRect.topleft = (10, WINDOWHEIGHT - 25)
+
+    while True:
+        DISPLAYSURF.fill(bgColor)
+        drawButtons()
+
+        DISPLAYSURF.blit(infoSurf, infoRect)
+
+        checkForQuit()
+
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def checkForQuit():
+    for event in pygame.event.get(QUIT):  # get all the QUIT events
+        terminate()  # terminate if any QUIT events are present
+    for event in pygame.event.get(KEYUP):  # get all the KEYUP events
+        if event.key == K_ESCAPE:
+            terminate()  # terminate if the KEYUP event was for the Esc key
+        pygame.event.post(event)  # put the other KEYUP event objects back
+
+def drawButtons():
+    pygame.draw.rect(DISPLAYSURF, YELLOW, YELLOWRECT)
+    pygame.draw.rect(DISPLAYSURF, BLUE,   BLUERECT)
+    pygame.draw.rect(DISPLAYSURF, RED,    REDRECT)
+    pygame.draw.rect(DISPLAYSURF, GREEN,  GREENRECT)
+
+if __name__ == '__main__':
+    main()
